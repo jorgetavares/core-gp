5cf60965d45e32ee85e8afb0969c2027ec082682
Jorge Tavares <jorge.tavares@ieee.org>
2010-05-06 20:30:43 +0200

    Update of tree builder functions to the new function/terminal sets representation.
     - Full, Grow and Ramped-half-and-half methods now make use of the new
       representation of nodes for the funtion and terminal sets.
     - The implemented methods only allow depth as a limit. Future generalization
       to number of nodes in the tree is required.

--- src/tree.lisp
+++ src/tree.lisp
@@ -4,59 +4,46 @@
 ;;; tree generators and related functions
 ;;;
 
-(defun ramped-half-and-half (limit fset tset)
+(defun make-random-tree (max-size fset fset-size tset tset-size
+			 &key (method :ramped) (user nil))
+  "Return a random tree according to given method to a given size."
+  (case method
+    (:full (full-method-depth 0 max-size fset fset-size tset tset-size ))
+    (:grow (grow-method-depth 0 max-size fset fset-size tset tset-size ))
+    (:ramped (ramped-half-and-half 0 max-size fset fset-size tset tset-size))
+    (otherwise (funcall user 0 max-size fset fset-size tset tset-size))))
+
+(defun ramped-half-and-half (size limit fset fset-size tset tset-size)
   "A gp tree is created with half of probability for each method."
-  (let ((fset-size (length fset))
-	(tset-size (length tset)))
-    (if (< (random 1.0) 0.5)
-	(full-method-tree-generic 0 limit fset fset-size tset tset-size)
-	(grow-method-tree-generic 0 limit fset fset-size tset tset-size))))
+  (if (< (random 1.0) 0.5)
+      (full-method-depth size limit fset fset-size tset tset-size)
+      (grow-method-depth size limit fset fset-size tset tset-size)))
 
-(defun make-fset (&rest args)
-  (loop 
-     for function in args by #'cddr
-     for arguments in (rest args) by #'cddr
-     collect (cons function arguments)))
-
-(defun function-name (pair)
-  (car pair))
-
-(defun function-args (pair)
-  (cdr pair))
-
-(defun full-method-tree-generic (size limit fset fset-size tset tset-size)
-  "Random tree according to the Full method."
+(defun full-method-depth (size limit fset fset-size tset tset-size)
+  "Random tree using the Full method and depth as size."
   (if (= size limit)
       (process-terminal (nth (random tset-size) tset))
-      (let* ((function (nth (random fset-size) fset))
-	     (name (function-name function))
-	     (args (function-args function)))
-	(cons name
-	      (loop repeat args 
-		 collect (full-method-tree-generic 
+      (let ((function (nth (random fset-size) fset)))
+	(cons (operator function)
+	      (loop repeat (arity function) 
+		 collect (full-method-depth 
 			  (1+ size) limit fset fset-size tset tset-size))))))
 
-(defun grow-method-tree-generic (size limit fset fset-size tset tset-size)
-  "Random tree according to the Grow method."
+(defun grow-method-depth (size limit fset fset-size tset tset-size)
+  "Random tree suing the Grow method and depth as size."
   (if (= size limit)
       (process-terminal (nth (random tset-size) tset))
-      (let* ((set (append fset tset))
-	     (element (nth (random (+ fset-size tset-size)) set)))
-	(if (consp element)
-	    (let ((name (function-name element))
-		  (args (function-args element)))
-	      (cons name
-		    (loop repeat args 
-		       collect (grow-method-tree-generic 
+      (let ((index (random (+ fset-size tset-size))))
+	(if (< index fset-size) 
+	    (let ((function (nth index fset)))
+	      (cons (operator function)
+		    (loop repeat (arity function)
+		       collect (grow-method-depth
 				(1+ size) limit fset fset-size tset tset-size))))
-	    (process-terminal element)))))
-
+	    (process-terminal (nth (- index fset-size) tset))))))
+	     
 (defun process-terminal (terminal)
-  "Return a constant or a terminal function."
-  (case terminal
-    (gp-constant (gp-constant))
-    (gp-constant-int (gp-constant-int))
-    (gp-constant-real (gp-constant-real))
-    (gp-true (gp-true))
-    (gp-false (gp-false))
-    (otherwise (list terminal))))
+  "Process the type of terminal."
+  (if (ephemeral terminal)
+      (funcall (operator terminal))
+      (list (operator terminal))))
