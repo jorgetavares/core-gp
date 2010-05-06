a11318792525f62aeeb7f8b51688b5e9c4e2b225
Jorge Tavares <jorge.tavares@ieee.org>
2010-05-06 11:43:25 +0200

    Classes for individual and genomes.
     - An individual is now represented by a class, which contains slots
       for a unique id (must be supplied by an outside function), an object
       for the genome, a fitness value (later should be an object too) and
       a flag to indicate if evaluation is required.
     - Classes for several types of genomes should be defined. The basic
       genome only contains the slot for the actual genome (of any type).
     - For every applicationa proper genome and associated genetic oeprators
       must be defined. E.g., tree-genome contains a variable length list
       which represents a parse tree.
     - Other genomes must subclass genome and in principle just need to add
       slots that help its manipulation. In the case of tree-genome, a tree
       depth and a count of nodes gives the size of the tree, and provides
       necessary extra info for the algorithm. A bit genome could just have
       a length slot.
     - Note: at the moment there is no need for extra classes defining new
       individuals since all the major differences are contained in the genomes.

--- src/individual.lisp
+++ src/individual.lisp
@@ -4,6 +4,93 @@
 ;;; representation and population initialization
 ;;;
 
+;;;
+;;; base individual
+;;;
+
+(defclass individual ()
+  ((id
+    :initarg :id
+    :initform (error "Class individual: must supply a unique id.") 
+    :reader id
+    :documentation "Unique ID of the individual.")
+   (genome
+    :initarg :genome
+    :initform nil
+    :accessor genome
+    :documentation "Individual's genome (tree, permutation, etc).")
+   (fitness
+    :initarg :fitness
+    :initform nil
+    :accessor fitness
+    :documentation "Individual's fitness value.")
+   (eval-p
+    :initarg :eval-p
+    :initform t
+    :accessor eval-p
+    :documentation "Predicate which indicates if individual needs to be evaluated.")
+   ))
+
+
+;;;
+;;; genome definitions
+;;;
+
+(defclass genome ()
+  ((genome
+    :initarg :genome
+    :initform nil
+    :accessor genome
+    :documentation "Genome slot.")
+   ))
+
+(defclass tree-genome (genome)
+   ((tree-depth
+    :init-arg :tree-depth
+    :initform 0
+    :accessor tree-depth
+    :documentation "The individuals's tree depth.")
+   (nodes-count
+    :init-arg :nodes-count
+    :initform 0
+    :accessor nodes-count
+    :documentation "The amount of function and terminal nodes in the tree.")
+    ))
+
+(defclass linear-genome (genome)
+  ((size
+    :initarg :size
+    :initform 0
+    :accessor size
+    :documentation "Genome's current size.")
+   ))
+
+
+;;;
+;;; genome builders
+;;;
+
+(defun make-empty-tree-genome ()
+  (make-instance 'tree-genome))
+  
+(defun make-random-tree-genome ()
+  
+  )
+
+
+
+;;;
+;;; individual builders
+;;;
+
+(defun make-tree-individual ()
+
+  )
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defstruct individual
   (tree nil)
   (fitness 0)
