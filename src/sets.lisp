1cee2d532dd1d82c437d75490c52f066b9068ea7
Jorge Tavares <jorge.tavares@ieee.org>
2010-05-20 12:33:45 +0200

    Add class to manage Function and Terminal sets.
     - Class sets-container is the place holder for the function and
       terminal sets. Also contains additional information to make
       the use of the sets more simple.
     - Methods to do certain operations (e.g., get a random function)
       should be added at a later stage.
     - Additional containers can be created from this base class, for
       example, to have Strong-Type GP (quick access to functions by
       type) or other variants.

--- src/sets.lisp
+++ src/sets.lisp
@@ -1,6 +1,63 @@
 ;(in-package #:core-gp)
 
 ;;;
+;;; sets container
+;;;
+
+(defclass sets-container ()
+  ((functions
+    :initarg :functions 
+    :initform (error "Class sets-container: must provide a list with functions.")
+    :accessor functions
+    :documentation "Function Set.")
+   (terminals
+    :initarg :terminals 
+    :initform (error "Class sets-container: must provide a list with terminals.")
+    :accessor terminals
+    (:documentation "Terminal Set."))
+   (functions-size
+    :reader functions-size
+    :documentation "Number of elements in the Function Set.")
+   (terminals-size
+    :reader terminals-size
+    :documentation "Number of elements in the Terminal Set.")
+   (arity-table
+    :reader arity-table
+    :documentation "Function Set organized by muber of arguments.")))
+
+
+;;;
+;;; builder
+
+(defmethod initialize-instance :after ((container sets-container) &key)
+  (setf (slot-value container 'functions-size) 
+	(length (slot-value container 'functions)))
+  (setf (slot-value container 'terminals-size)
+	(length (slot-value container 'terminals)))
+  (setf (slot-value container 'arity-table)
+	(process-fset-arity (slot-value container 'functions))))
+  
+(defun make-sets-container (function-names terminal-names)
+  "Return a container for the Function and Terminal Sets (nodes must be defined)."
+  (make-instance 'sets-container
+		 :functions (make-set function-names)
+		 :terminals (make-set terminal-names)))
+
+(defun make-set (node-names)
+  "Return a list of object nodes that compose the functino/terminal set."
+  (mapcar #'make-instance node-names)) ;; TODO: error check for undefined nodes
+
+(defun process-fset-arity (fset)
+  "Group the functions of the set according to the arity."
+  (loop with arity-table = (make-hash-table) 
+     for node in fset
+     do (let ((nodes (gethash (arity node) arity-table)))
+	  (setf (gethash (arity node) arity-table) 
+		(push node nodes)))
+     finally (return arity-table)))
+
+
+;;;
 ;;; function and terminal set definition/creation
 ;;;
 
@@ -16,19 +73,6 @@
 	     `(arity     :initform ,(length `,args) :reader arity))
 	(string :initform ,string :reader string-form)))))
 
-(defun make-set (node-names)
-  "Return a list of object nodes that compose the functino/terminal set."
-  (mapcar #'make-instance node-names))
-
-(defun process-fset-arity (fset)
-  "Group the functions of the set according to the arity."
-  (loop with arity-table = (make-hash-table) 
-     for node in fset
-     do (let ((nodes (gethash (arity node) arity-table)))
-	  (setf (gethash (arity node) arity-table) 
-		(push node nodes)))
-     finally (return arity-table)))
-
 
 ;;;;
 ;;;; function and terminal sets lists to be used for evolution
@@ -36,7 +80,6 @@
 
 ;;;
 ;;; function set definitions
-;;;
 
 ;; math
 (defnode gp-plus ((a b) (:string "+"))
@@ -118,10 +161,8 @@
 
 ;;;
 ;;; terminal set definitions
-;;;
 
 ;; constants (only used in a tree generation by keeping their values)
-
 
 ;; allows a user-defined function to generate constants
 ;; - store fucntion in *generate-constant*
